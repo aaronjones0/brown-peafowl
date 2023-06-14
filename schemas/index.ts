fd780d607baed33f1c9697861b5caeb558468ba7
aaronjones1 <126199236+aaronjones0@users.noreply.github.com>
2023-06-14 11:06:29 -0400

    Add TV Series

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import tvSeries from "./tv-series"
+
+export const schemaTypes = [tvSeries]
---
+++ schemas/tv-series.ts
@@ -0,0 +1,12 @@
+export default {
+  name: 'tvSeries',
+  type: 'document',
+  title: 'TV Series',
+  fields: [
+    {
+      name: 'short_title',
+      type: 'string',
+      title: 'Short Title',
+    },
+  ],
+}
